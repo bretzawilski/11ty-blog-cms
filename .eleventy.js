8dd2b8f432a5b68617e4da84255b4076372b9c4c
Bret Zawilski <bret.zawilski@gmail.com>
2021-07-02 14:40:50 +0100

    Blog works

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+const moment = require("moment");
+
+moment.locale("en");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+
+  eleventyConfig.addFilter("dateIso", (date) => {
+    return moment(date).toISOString();
+  });
+
+  eleventyConfig.addFilter("dateReadable", (date) => {
+    return moment(date).utc().format("LL"); // E.g. May 31, 2019
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
